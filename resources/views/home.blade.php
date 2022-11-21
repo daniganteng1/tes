021832294114ab2f7b89091efae8faeb47077e4f
daniganteng1 <adhaninurrokhim@gmail.com>
2022-11-21 11:23:23 +0700

    iti

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            background-color: blue
+        }
+        h1{
+            text-align:end;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        Hello
+    </h1>
+</body>
+</html>
